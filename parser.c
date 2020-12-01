724746493d88c75e908cd18396e9cebc525ebd77
Thomasorus <contact@thomasorus.com>
2020-12-01 23:37:02 +0100

    Things i guess?

--- parser.c
+++ parser.c
@@ -2,40 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 // OLD TEST WILL REDO LATER
 // void parser(char** str) {
 //      *str = "lol";
 
-     // char rules[2][20] = { 
-     //      "/(#+)(.*)/g",
-     //      "/`{3,}(?!.*`)/g"
-     //       };
-     // int len = sizeof(rules)/sizeof(rules[0]);
-     // printf("%d\n", len);
+// char rules[2][20] = {
+//      "/(#+)(.*)/g",
+//      "/`{3,}(?!.*`)/g"
+//       };
+// int len = sizeof(rules)/sizeof(rules[0]);
+// printf("%d\n", len);
 
-     // int i = 0;
-     // for(i = 0; i < len; i++){
-     //      printf("%d\n", i);
-     //      printf("%s\n", rules[i]);
-     // }
+// int i = 0;
+// for(i = 0; i < len; i++){
+//      printf("%d\n", i);
+//      printf("%s\n", rules[i]);
+// }
 // }
 
-void readFile(char** path, char** textContent) {
+void readFile(char **path, char **textContent)
+{
      FILE *file;
      file = fopen(*path, "r");
-     if(file == NULL)
+     if (file == NULL)
      {
-          printf("Error!");   
-          exit(1);             
+          printf("Error!");
+          exit(1);
      }
 
      char buffer[10];
-     char *input = 0; //Size of input is unknow and will be changed
+     char *input = 0;    //Size of input is unknow and will be changed
      size_t cur_len = 0; //size_t is an unsigned integer. This type is used to represent the size of an object
 
-     //Reading file
-     while (fgets(buffer, sizeof(buffer), file) != 0) {
+     // Reading file
+     while (fgets(buffer, sizeof(buffer), file) != 0)
+     {
           size_t buf_len = strlen(buffer);
           char *extra = realloc(input, buf_len + cur_len + 1);
           if (extra == 0)
@@ -44,31 +45,39 @@ void readFile(char** path, char** textContent) {
           strcpy(input + cur_len, buffer);
           cur_len += buf_len;
      }
-     free(input);
      *textContent = input;
+     free(input);
      fclose(file);
 }
 
-void splitText(char** text) {
-     char delim[] = "\n";
-     //strtok modifies the original string. It puts NULL characters ('\0') at the delimiter position after every call to strtok so that tokens can be tracked. strtok also internally remembers the next token's starting position.
-     char *ptr = strtok(*text, delim);
-
-     //In the next call to strtok, the first parameter needs to be NULL so that strtok starts splitting the string from the next token's starting position it remembers.
+void splitText(char **text, char **delimiter, char **splittedText)
+{
+     char *ptr = strtok(*text, *delimiter);
      int i;
      for (i = 0; ptr != NULL; i++)
-	{
-          printf("%s \n", ptr);
-		ptr = strtok(NULL, delim);
-	}
+     {
+          printf("Adding line: %s \n", ptr);
+          strcpy(*splittedText, ptr);
+          ptr = strtok(NULL, *delimiter);
+     }
+     free(ptr);
 }
 
-int main(void) {
-     char* path = "demo.kaku";
-     char* textContent;
-     readFile(&path, &textContent);
+int main(void)
+{
+     // char *path = "demo.kaku";
 
-     splitText(&textContent);
+     // Recover text file and get the number of lines
+     // char *textContent;
+     // readFile(&path, &textContent);
+     // int contentLength = *textContent;
+     // printf("Number of lines with empty text: %d \n", contentLength);
+     char *textContent = "Some\nMultiline\ntext";
+
+     char *splittedText;
+     char *delimiter = "\n";
+     splitText(&textContent, &delimiter, &splittedText);
+     printf("Number of lines without empty text:%d \n", *splittedText);
 
      return 0;
 }
